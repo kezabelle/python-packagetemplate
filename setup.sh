3da1e112b45d9e4ff17fb48821bf62f0aada8223
Keryn Knight <keryn@kerynknight.com>
2017-01-10 15:13:32 +0000

    Fix where the .gitignore goes.

--- setup.sh
+++ setup.sh
@@ -28,4 +28,4 @@ mv "$DIR/packagename" "$DIR/$PACKAGECHOICE"
 rm "$DIR/setup.sh"
 rm "$DIR/README.md"
 git init $DIR
-printf '%s\n' 'htmlcov' 'dist' '*.pyo' '*.pyc''.coverage' '*.egg*' > .gitignore
+printf '%s\n' 'htmlcov' 'dist' '*.pyo' '*.pyc''.coverage' '*.egg*' > "$DIR/.gitignore"
